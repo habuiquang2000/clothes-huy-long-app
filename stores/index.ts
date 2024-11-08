9a896cc5e08dde51a38b2d3bb05fe7ce0df3abbb
habuiquang2000 <habuiquang2000@gmail.com>
2024-11-08 08:38:30 +0700

    [State] Add Redux toolkit

---
+++ stores/index.ts
@@ -0,0 +1,33 @@
+import { combineSlices, configureStore } from "@reduxjs/toolkit";
+
+import { userSlice } from "./features/user/userSlice";
+import { categoryListSlice } from "./features/category/categorySlice";
+import { productListSlice } from "./features/product/productListSlice";
+import { cartSlice } from "./features/cart/cartSlice";
+
+import type { Action, ThunkAction } from "@reduxjs/toolkit";
+
+const rootReducer = combineSlices(
+  userSlice,
+  categoryListSlice,
+  productListSlice,
+  cartSlice
+);
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+// Infer the return type of `makeStore`
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = AppStore["dispatch"];
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>;

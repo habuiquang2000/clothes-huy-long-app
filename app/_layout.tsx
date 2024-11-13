f055c17be18968a1ce4e9667d297a113ed4490f5
habuiquang2000 <habuiquang2000@gmail.com>
2024-11-13 10:29:55 +0700

    [Source] Update Newest Expo Version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import { StoreProvider } from "./StoreProvider";
+import { StoreProvider } from "@/components/StoreProvider";
 
 // console.reportErrorsAsExceptions = false;
 
--- components/StoreProvider.tsx
+++ components/StoreProvider.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import type { ReactNode } from "react";
+import type { PropsWithChildren, ReactNode } from "react";
 import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 
 import type { AppStore } from "@/stores";
 import { makeStore } from "@/stores";
 
-interface Props {
-  readonly children: ReactNode;
-}
-
-export const StoreProvider = ({ children }: Props) => {
+export const StoreProvider = ({ children }: PropsWithChildren) => {
   const storeRef = useRef<AppStore | null>(null);
 
   if (!storeRef.current) {
